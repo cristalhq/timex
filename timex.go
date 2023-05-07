1ae6701c1fc845e779821f84a57b5a76216e6d88
Oleg Kovalov <oleg@hey.com>
2023-05-07 18:00:51 +0200

    Add Now

--- timex.go
+++ timex.go
@@ -5,6 +5,11 @@ import "time"
 // Duration is an alias for time.Duration.
 type Duration = time.Duration
 
+// Now returns currect time in UTC.
+func Now() time.Time {
+	return time.Now().UTC()
+}
+
 // StopTimer in a proper manner.
 func StopTimer(t time.Timer) {
 	if !t.Stop() {
