97a64fe437b65e5a290eb5ed8f3f8db55c896b3e
Oleg Kovalov <oleg@hey.com>
2023-05-21 13:17:13 +0200

    Timestamp from unix time

--- timestamp.go
+++ timestamp.go
@@ -10,9 +10,23 @@ type Timestamp struct {
 	time.Time
 }
 
+// TimestampFromString returns Timestamp fomr the string representing Unix time.
+func TimestampFromString(s string) (Timestamp, error) {
+	msec, err := strconv.ParseInt(s, 10, 64)
+	if err != nil {
+		return Timestamp{}, err
+	}
+	return TimestampFromMilli(msec), nil
+}
+
+// TimestampFromMilli returns Timestamp from the Unix time in milliseconds.
+func TimestampFromMilli(msec int64) Timestamp {
+	return AsTimestamp(time.UnixMilli(msec))
+}
+
 // AsTimestamp returns time.Time wrapped as Timestamp.
 func AsTimestamp(t time.Time) Timestamp {
-	return Timestamp{Time: t}
+	return Timestamp{Time: t.UTC()}
 }
 
 // MarshalJSON implements json.Marshaler interface.
@@ -22,11 +36,11 @@ func (ts Timestamp) MarshalJSON() ([]byte, error) {
 
 // UnmarshalJSON implements json.Unmarshaler interface.
 func (ts *Timestamp) UnmarshalJSON(b []byte) error {
-	ms, err := strconv.ParseInt(string(b), 10, 64)
+	msec, err := strconv.ParseInt(string(b), 10, 64)
 	if err != nil {
 		return err
 	}
 
-	*ts = AsTimestamp(time.UnixMilli(ms))
+	*ts = AsTimestamp(time.UnixMilli(msec))
 	return nil
 }
